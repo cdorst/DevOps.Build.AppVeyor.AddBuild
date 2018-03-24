a61d19c9784e42dfae1348bf540b1301837397c6
Christopher Dorst <chris@dorst.email>
2018-03-24 15:46:59 -0500

    Add BuildAdder.cs

---
+++ DevOps.Build.AppVeyor.AddBuild/BuildAdder.cs
@@ -0,0 +1,17 @@
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading.Tasks;
+using static DevOps.Build.AppVeyor.AzureStorageTableLedger.Builder.AppveyorBuildTableHelper;
+using static DevOps.Build.AppVeyor.GetAzureTable.AzureTableGetter;
+
+namespace DevOps.Build.AppVeyor.AddBuild
+{
+    /// <summary>Function adds the given repository build information to an Azure Storage Table ledger</summary>
+    public static class BuildAdder
+    {
+        /// <summary>Adds a table entry to an Azure Table named "appveyor"</summary>
+        public static async Task AddBuildAsync(string name, string version, string dependencies = null, string fileHashes = null)
+        {
+            var entry = BuildTableEntry(name, version, dependencies, fileHashes); var operation = TableOperation.InsertOrReplace(entry); var table = await GetTable(); await table.ExecuteAsync(operation);
+        }
+    }
+}
